ff3d9526bda8a5339cc032fb640a2e5ec1c04044
BenjaminNassW <benjanass@gmail.com>
2022-03-04 00:51:21 +0000

    tarea falta color jumbo y footer

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbo() {
+	return (
+		<div className="container-fluid py-5">
+			<h1 className="display-5 fw-bold">Custom jumbotron</h1>
+			<p className="col-md-8 fs-4">Using</p>
+			<button className="btn btn-primary btn-lg" type="button">
+				Example button
+			</button>
+		</div>
+	);
+}
+export default Jumbo;
